d5a1782ebb46f04abb338898f1c85e3a6918c9e6
farissbayu <bayustfa@gmail.com>
2025-02-20 22:30:20 +0700

    feat: login action and auth route protection

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,9 +1,14 @@
+"use client";
+
 import { Button, Input } from "@heroui/react";
-import React from "react";
+import React, { useActionState } from "react";
 import { ButtonOauthGoogle } from "../_components/button-oauth-google";
 import Link from "next/link";
+import { loginAction } from "./action";
 
 export default function Page() {
+  const [state, formAction, pending] = useActionState(loginAction, null);
+
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="p-8 rounded-xl border">
@@ -16,10 +21,11 @@ export default function Page() {
         <p className="text-sm text-slate-600 mt-4 mb-2">
           Please login first to continue!
         </p>
-        <form className="space-y-4">
+        <form className="space-y-4" action={formAction}>
           <Input
             label="Email"
             type="email"
+            name="email"
             variant="underlined"
             placeholder="Enter your email"
             className="min-w-64"
@@ -27,6 +33,7 @@ export default function Page() {
           <Input
             label="Password"
             type="password"
+            name="password"
             variant="underlined"
             placeholder="********"
             className="min-w-64"
@@ -34,10 +41,17 @@ export default function Page() {
           <Button
             className="w-full bg-cyan-700 text-white font-semibold"
             radius="lg"
+            type="submit"
+            isDisabled={pending}
           >
             Login
           </Button>
         </form>
+        {state?.success === false && (
+          <p className="text-xs text-red-500 text-center my-2">
+            {state?.message}
+          </p>
+        )}
         <hr className="my-4" />
         <ButtonOauthGoogle />
         <p className="text-sm mt-8 text-center">

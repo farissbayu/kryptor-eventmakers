a54ba6f3c4b3c1dc2972bc070917dea29bf2f100
ooary <aryinteristi@gmail.com>
2025-02-22 22:05:19 +0700

    Add Event Details Page and Join Event

---
+++ src/app/(homepage)/[id]/page.js
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import JoinSection from "./_components/joinSection";
+import { prisma } from "@/utils/prisma";
+import moment from "moment";
+
+export default async function page({ params }) {
+  const { id } = await params;
+  const event = await prisma.event.findFirst({
+    where: {
+      id,
+    },
+    include: {
+      author: true,
+      participants: true,
+    },
+  });
+  const eventDate = moment(event?.eventDate).calendar();
+  const createdDate = moment(event?.createdAt).calendar();
+
+  return (
+    <div className="block space-y-10 p-10 w-full">
+      <section className="space-y-2">
+        <h1 className="text-2xl font-semibold">{event?.name}</h1>
+        <div className="flex space-x-2">
+          <Image />
+        </div>
+        <h1 className="text-medium text-slate-500">{event?.description}</h1>
+        <h1 className="text-sm text-slate-500">Event Start {eventDate}</h1>
+        <h1 className="text-sm text-slate-500">Quota : {event?.capacity}</h1>
+        <h1 className="text-sm text-slate-500">
+          Remaining Quota : {event?.capacity - event.participants.length}
+        </h1>
+
+        <h1 className="text-sm text-slate-500">
+          Registered : {event?.participants.length}
+        </h1>
+
+        <h1 className="text-sm text-slate-500">
+          {event?.place} | {event?.author.name} | Posted {createdDate}
+        </h1>
+      </section>
+      <JoinSection id={id} />
+    </div>
+  );
+}

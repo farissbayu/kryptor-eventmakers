1c9e0d32266cf500f313bceedfdcf5a333e0a61b
ooary <aryinteristi@gmail.com>
2025-02-22 14:23:15 +0700

    Wrapped Card Event with Link

--- src/app/(homepage)/discover/page.js
+++ src/app/(homepage)/discover/page.js
@@ -30,45 +30,51 @@ export default function page() {
         </div>
       </section>
       <section className="grid grid-cols-4 gap-5 ">
-        <Card className="max-w-[500px] shadow-sm">
-          <CardHeader>
-            <h1 className="font-semibold text-sm">
-              Jakarta | AI-Builder Co-working - 22 Feb
-            </h1>
-          </CardHeader>
-          <Divider />
-          <CardBody className="flex">
-            <p className="text-slate-300 text-sm">Today , 06:00 PM </p>
-            <p className="text-slate-300 text-sm">Trinity Tower - Ary P</p>
-          </CardBody>
-          <Divider />
-        </Card>
-        <Card className="max-w-[500px] shadow-sm">
-          <CardHeader>
-            <h1 className="font-semibold text-sm">
-              Jakarta | AI-Builder Co-working - 22 Feb
-            </h1>
-          </CardHeader>
-          <Divider />
-          <CardBody className="flex">
-            <p className="text-slate-300 text-sm">Today , 06:00 PM </p>
-            <p className="text-slate-300 text-sm">Trinity Tower - Ary P</p>
-          </CardBody>
-          <Divider />
-        </Card>
-        <Card className="max-w-[500px] shadow-sm">
-          <CardHeader>
-            <h1 className="font-semibold text-sm">
-              Jakarta | AI-Builder Co-working - 22 Feb
-            </h1>
-          </CardHeader>
-          <Divider />
-          <CardBody className="flex">
-            <p className="text-slate-300 text-sm">Today , 06:00 PM </p>
-            <p className="text-slate-300 text-sm">Trinity Tower - Ary P</p>
-          </CardBody>
-          <Divider />
-        </Card>
+        <Link href={"/"}>
+          <Card className="max-w-[500px] shadow-sm">
+            <CardHeader>
+              <h1 className="font-semibold text-sm">
+                Jakarta | AI-Builder Co-working - 22 Feb
+              </h1>
+            </CardHeader>
+            <Divider />
+            <CardBody className="flex">
+              <p className="text-slate-300 text-sm">Today , 06:00 PM </p>
+              <p className="text-slate-300 text-sm">Trinity Tower - Ary P</p>
+            </CardBody>
+            <Divider />
+          </Card>
+        </Link>
+        <Link href={"/"}>
+          <Card className="max-w-[500px] shadow-sm">
+            <CardHeader>
+              <h1 className="font-semibold text-sm">
+                Jakarta | AI-Builder Co-working - 22 Feb
+              </h1>
+            </CardHeader>
+            <Divider />
+            <CardBody className="flex">
+              <p className="text-slate-300 text-sm">Today , 06:00 PM </p>
+              <p className="text-slate-300 text-sm">Trinity Tower - Ary P</p>
+            </CardBody>
+            <Divider />
+          </Card>
+        </Link>
+        <Link href={"/"}>
+          <Card className="max-w-[500px] shadow-sm">
+            <CardHeader>
+              <h1 className="font-semibold text-sm">
+                Jakarta | AI-Builder Co-working - 22 Feb
+              </h1>
+            </CardHeader>
+            <Divider />
+            <CardBody className="flex">
+              <p className="text-slate-300 text-sm">Today , 06:00 PM </p>
+              <p className="text-slate-300 text-sm">Trinity Tower - Ary P</p>
+            </CardBody>
+            <Divider />
+          </Card>
+        </Link>
       </section>
     </div>
   );

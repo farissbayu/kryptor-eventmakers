f40da20c45c08a14c3b11574bc7b94762b7d3bfb
farissbayu <bayustfa@gmail.com>
2025-02-24 10:03:13 +0700

    fix: add redirect uri for production

--- src/utils/arctic.js
+++ src/utils/arctic.js
@@ -2,6 +2,9 @@ import * as arctic from "arctic";
 
 const clientId = process.env.GOOGLE_CLIENT_ID;
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-const redirectURI = process.env.GOOGLE_REDIRECT_URI;
+const redirectURI =
+  process.env.NODE_ENV === "production"
+    ? process.env.GOOGLE_REDIRECT_URI
+    : process.env.GOOGLE_REDIRECT_URI_DEV;
 
 export const google = new arctic.Google(clientId, clientSecret, redirectURI);
